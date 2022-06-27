35c0f081d8f005f3417dc952dc1048ebbdd9cfa0
Ivan Ivanov <IvanIvanov@example.com>
2022-06-27 20:41:52 +0300

    Èçìåíåíèå ïðèâåòñòâèÿ

---
+++ CMakeHelloWorld.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeHelloWorld.cpp: определяет точку входа для приложения.
+//
+
+#include "CMakeHelloWorld.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake and Git." << endl;
+	return 0;
+}
