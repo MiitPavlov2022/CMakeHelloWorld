afe138ec9a8c2c3062b6cd09ad2f0f612e702d7f
Ivan Ivanov <IvanIvanov@example.com>
2022-07-02 18:55:33 +0300

    Тестирование веток

--- CMakeHelloWorld.cpp
+++ CMakeHelloWorld.cpp
@@ -5,7 +5,10 @@
 
 using namespace std;
 
+string concat(string s1, string s2) { return s1 + s2; }
+
 int main() {
 	cout << "Hello CMake and Git." << endl;
+    cout << concat("Test ", " branch.") << endl;
 	return 0;
 }
